a1da10a34c03c1939d288dbe0708cf6c5c89aee3
trabalhoand19-bit <trabalhoand19@gmail.com>
2026-01-16 10:30:31 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById("formCurso").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Inscrição realizada com sucesso! Verifique seu e-mail.");
+});
